115fe27350256405568d9a29daec135555f337c1
sbasteiro <sbasteiro1@gmail.com>
2023-05-18 18:57:59 -0300

    fix: change in the ajax so that it works on the server

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,35 @@
 function shippingDelete(shippingId) {
+    const url = '/api/softDeleteShipping/' + shippingId;
     if (confirm('¿Estás seguro de borrar el envío?')) {
-        $.ajax({
-            url: "/api/softDeleteShipping/"+ shippingId,
-            type: "DELETE",
-            context: document.body
-        }).done(function() {
-            alert('Envío eliminado');
-            window.location.reload(true);
-        });
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message);
+            })
+        alert('Envío eliminado');
+        window.location.reload(true);
     } else {
         alert('Eliminación cancelada');
     }
 }
 
 function shippingUpdate(shippingId) {
-        $.ajax({
-            url: "/api/shippingUpdate/"+ shippingId,
-            type: "PATCH",
-            context: document.body
-        }).done(function() {
-            window.location.reload(true);
-        });
+    const url = '/api/shippingUpdate/' + shippingId;
+    fetch(url, {
+        method: 'PATCH',
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message);
+        })
+    alert('Envío eliminado');
+    window.location.reload(true);
 }
